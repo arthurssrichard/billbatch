e7522aa5d97ead1f34e0fdc8a63e59f9cdf1949d
arthurssrichard <arthurssrichard@gmail.com>
2026-08-07 18:11:33 -0300

    feat: add migration and empty model for logs table

---
+++ database/migrations/2026_08_07_210602_create_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Empresa;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Empresa::class)->constrained()->cascadeOnDelete();
+            $table->string('status'); // sucess, warning, info, error
+            $table->string('arquivo_origem')->nullable();
+            $table->string('nome');
+            $table->string('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
