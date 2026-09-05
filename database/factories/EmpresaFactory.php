6473961ae339c2468027a9751252da391f0c8d17
arthurssrichard <arthurssrichard@gmail.com>
2026-09-05 01:42:28 -0300

    feat: add Empresa factory

---
+++ database/factories/EmpresaFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Empresa;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Empresa>
+ */
+class EmpresaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'usuario_id' => \App\Models\Usuario::factory(),
+            'nome' => fake()->company(),
+        ];
+    }
+}
