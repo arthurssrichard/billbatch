c6e423c389ab9ae271cd8c40d63f392f5bd79778
arthurssrichard <arthurssrichard@gmail.com>
2026-09-05 19:42:33 -0300

    feat: add Contato factory

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contato extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['endereco_email'];
 
     public function cliente(): BelongsTo
