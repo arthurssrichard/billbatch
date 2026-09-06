8197912d34cf1f7223becf4aacce69d5cf56129e
arthurssrichard <arthurssrichard@gmail.com>
2026-09-06 13:53:45 -0300

    feat: add ModeloMensagemCobranca factory

--- app/Models/ModeloMensagemCobranca.php
+++ app/Models/ModeloMensagemCobranca.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use App\Enums\TipoCobranca;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Empresa;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ModeloMensagemCobranca extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['tipo', 'assunto', 'corpo'];
 
     protected function casts(): array
