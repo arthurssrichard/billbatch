4d55195293ee52e9aca173c3f2306307f28d7ed7
arthurssrichard <arthurssrichard@gmail.com>
2026-09-18 10:16:13 -0300

    wip: debug scope not filtering on empresas.index

--- app/Livewire/Empresas/Index.php
+++ app/Livewire/Empresas/Index.php
@@ -9,6 +9,11 @@ class Index extends Component
 {
     public function render()
     {
+        dd(
+            app()->bound(\App\Models\Usuario::class) ? app(\App\Models\Usuario::class)->id : 'não bound',
+            Empresa::pluck('id', 'usuario_id')
+        );
+
         return view('livewire.empresas.index', [
             'empresas' => Empresa::withCount('clientes')->get(),
         ])->layout('components.layout', ['title' => 'Empresas']);
