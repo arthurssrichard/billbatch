0bd90c1fc53172748f55fc5f539e342232577fec
arthurssrichard <arthurssrichard@gmail.com>
2026-09-18 10:28:37 -0300

    wip: pint formatting

--- tests/Feature/Livewire/Empresas/IndexTest.php
+++ tests/Feature/Livewire/Empresas/IndexTest.php
@@ -12,8 +12,6 @@ test('empresas.index mostra só as empresas do visitante atual', function () {
 
     $pagina = $this->withCookie('visitor_token', $usuarioA->uuid)->get('/empresas');
 
-
-    
     $pagina->assertSee($empresaA->nome);
     $pagina->assertDontSee($empresaB->nome);
 });
